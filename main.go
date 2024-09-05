84563f2c8ddf2d8445a1eac49c4e1e3ff61189eb
JoaoPaulo-creator <joaosarto@gmail.com>
2024-09-05 00:52:14 -0300

    test: reading a file

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
 
+	data, err := os.ReadFile("test.lang")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	parsedData := string(data)
+	result := strings.ReplaceAll(parsedData, " ", "")
+
+	for _, r := range result {
+		if string(r) == "+" {
+
+		}
+		fmt.Println(string(r))
+	}
 }
